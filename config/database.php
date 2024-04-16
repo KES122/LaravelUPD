88c1d44f9f5fdbed5361ddb877cb513fdefb614a
Elisey <kameko-es@mail.ru>
2024-04-16 15:12:56 +0300

    Config changes for correct project startup

--- config/database.php
+++ config/database.php
@@ -84,7 +84,7 @@ return [
             'url' => env('DB_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'task_management'),
+            'database' => env('DB_DATABASE', 'task_management_laravel_test'),
             'username' => env('DB_USERNAME', 'postgres'),
             'password' => env('DB_PASSWORD', 'toor'),
             'charset' => env('DB_CHARSET', 'utf8'),
